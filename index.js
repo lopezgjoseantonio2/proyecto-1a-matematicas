5f2d28956b47606dd4c4b94dc474bd0d3013df91
Jose <lopezgjoseantonio2@gmail.com>
2022-07-07 11:41:26 +0200

    validar numeros

--- index.js
+++ index.js
@@ -6,15 +6,25 @@ module.exports = {
      * @returns Suma de los números
      */
     suma: function(n1,n2){
-        return n1+n2;
+        return (this.esNumero(n1,n2)? n1 + n2 : this.mensajeError());
     },
     resta: function(n1,n2){
-        return n1-n2;
+        return (this.esNumero(n1,n2)? n1 - n2 : this.mensajeError());
     },
     multiplicacion: function(n1,n2){
-        return n1*n2;
+        return (this.esNumero(n1,n2)? n1 * n2 : this.mensajeError());
     },
     division: function(n1,n2){
-        return n1/n2;
+        return (this.esNumero(n1,n2)? n1 / n2 : this.mensajeError());
+    },
+
+    mensajeError: function(){
+        console.log('Un valor o los dos valores no son numeros');
+    },
+    esNumero: function(n1,n2){
+        if (typeof n1 !== 'number' || typeof n2 !=='number'){
+            return false;
+        }
+        return true;
     }
 }
--- prueba.js
+++ prueba.js
@@ -1,6 +1,6 @@
 const m=require('.');
 
 console.log(m.suma(5,7));
-console.log(m.resta(5,7));
+console.log(m.resta(true ,7));
 console.log(m.multiplicacion(5,7));
 console.log(m.division(5,7));
