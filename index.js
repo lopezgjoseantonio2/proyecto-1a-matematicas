b822e3524c94ec5b02225adfe22a6a23cb6fce56
Jose <lopezgjoseantonio2@gmail.com>
2022-07-07 11:16:58 +0200

    primera version

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+    /**
+     * 
+     * @param {*} n1 Numero 1 de la suma
+     * @param {*} n2 Numero 2 de la suma
+     * @returns Suma de los números
+     */
+    suma: function(n1,n2){
+        return n1+n2;
+    },
+    resta: function(n1,n2){
+        return n1-n2;
+    },
+    multiplicacion: function(n1,n2){
+        return n1*n2;
+    },
+    division: function(n1,n2){
+        return n1/n2;
+    }
+}
